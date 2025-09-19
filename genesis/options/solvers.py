62d66e3d28ed2755924336e3e5f6a4f6799f9d52
Timmmothy <bank.of.thomas@gmail.com>
2025-09-19 14:26:48 -0400

    init new mpm solver, add use_legacy_solver option

--- genesis/options/solvers.py
+++ genesis/options/solvers.py
@@ -392,6 +392,8 @@ class MPMOptions(Options):
         This option is deprecated.
     leaf_block_size : int, optional
         This option is deprecated.
+    use_legacy_solver : bool, optional
+        Whether to use the legacy MPM solver. Defaults to True.
     """
 
     dt: Optional[float] = None
@@ -407,6 +409,8 @@ class MPMOptions(Options):
     # Deprecated sparse computation parameter.
     use_sparse_grid: bool = False
     leaf_block_size: int = 8
+
+    use_legacy_solver: bool = True
 
     def __init__(self, **data):
         super().__init__(**data)
